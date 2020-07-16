27f43a7da295b5eabc174ebd6dcedc5a5af9c3b2
Cleiton Silva <cleiton_.98@hotmail.com>
2020-07-15 22:18:31 -0300

    #111 - First commit, componentes tela login ok

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import 'bootswatch/dist/flatly/bootstrap.css';
+import React from 'react';
+
+
+
+class Card extends React.Component {
+
+    render() {
+        return (
+            <div className="card mb-3">
+                <h3 className="card-header">{this.props.title}</h3>
+                <div className="card-body">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card;
